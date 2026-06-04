1fe0e739c1b3d5ca680bfe4e448324b541aeb8b5
nikshay17 <nikshaykataria3@gmail.com>
2026-06-04 19:23:17 +0530

    Day 1 - project setup, DB connected, User model, UserRepository

---
+++ backend/src/main/java/com/prepVector/backend/model/User.java
@@ -0,0 +1,26 @@
+package com.prepVector.backend.model;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role = "USER";
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
---
+++ backend/src/test/java/com/prepVector/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prepVector.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
